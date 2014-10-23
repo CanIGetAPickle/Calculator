3bc7738af528fe73678d6ede723a2554c09954b9
Dan Santos <danielrobertsantos@gmail.com>
2014-10-23 13:45:12 -0400

    Update to validate inputs as integers.

---
+++ calculator2.rb
@@ -0,0 +1,30 @@
+num1 = ""
+until num1.to_i.to_s == num1
+	puts ">>> Enter your first number:"
+	num1 = gets.chomp
+end
+
+num2 = ""
+until num2.to_i.to_s == num2
+	puts ">>> Enter your second number:"
+	num2 = gets.chomp
+end
+
+num1 = num1.to_f
+num2 = num2.to_f
+
+puts ">>> Enter the number of the operation you wish to apply: 1. Add 2. Subtract 3. Multiply 4. Divide"
+operation = gets.chomp
+
+if operation == "1"
+	puts ">>> #{num1 + num2}"
+elsif operation == "2"
+	puts ">>> #{num1 - num2}"
+elsif operation == "3"
+	puts ">>> #{num1 * num2}"
+elsif operation == "4"
+	puts ">>> #{num1 / num2}"
+else
+	puts ">>> Sorry, that option doesn't exist!"
+end
+
