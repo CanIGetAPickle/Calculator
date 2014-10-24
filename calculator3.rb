2ba165671b8511f1b36ac5c2d6911a527d8f20c0
Dan Santos <danielrobertsantos@gmail.com>
2014-10-24 03:22:01 -0400

    Consolidated duplicate code into methods.

--- calculator3.rb
+++ calculator3.rb
@@ -1,22 +1,18 @@
 def validate_input(num)
-	if (num.to_i.to_s == num) || (num.to_f.to_s == num)
-		return true
-	else
-		return false
-	end
+  (num.to_i.to_s == num) || (num.to_f.to_s == num)
 end
 
-num1 = ""
-while validate_input(num1) == false do
-	puts ">>> Enter your first number:"
-	num1 = gets.chomp
+def get_user_input
+  number = gets.chomp until validate_input(number)
+  return number
 end
 
-num2 = ""
-while validate_input(num2) == false do
-	puts ">>> Enter your second number:"
-	num2 = gets.chomp
-end
+  puts ">>> Enter your first number:"
+  num1 = get_user_input
+
+  puts ">>> Enter your second number:"
+  num2 = get_user_input
+
 
 num1 = num1.to_f
 num2 = num2.to_f
@@ -25,14 +21,14 @@ puts ">>> Enter the number of the operation you wish to apply: 1. Add 2. Subtrac
 operation = gets.chomp
 
 if operation == "1"
-	puts ">>> #{num1 + num2}"
+  puts ">>> #{num1 + num2}"
 elsif operation == "2"
-	puts ">>> #{num1 - num2}"
+  puts ">>> #{num1 - num2}"
 elsif operation == "3"
-	puts ">>> #{num1 * num2}"
+  puts ">>> #{num1 * num2}"
 elsif operation == "4"
-	puts ">>> #{num1 / num2}"
+  puts ">>> #{num1 / num2}"
 else
-	puts ">>> Sorry, that option doesn't exist!"
+  puts ">>> Sorry, that option doesn't exist!"
 end
 
